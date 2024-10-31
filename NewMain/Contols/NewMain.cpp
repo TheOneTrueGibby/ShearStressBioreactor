80d53289ec6aeb20301eb884a151985597a1d4bf
Gibby <gibsonmoseley@gmail.com>
2024-10-31 10:59:36 -0700

    Adding More Definitions

--- NewMain/Contols/NewMain.cpp
+++ NewMain/Contols/NewMain.cpp
@@ -14,3 +14,14 @@
 #define PUMP_PIN 14
 #define STEPPER_STEP_PIN 15
 #define STEPPER_DIR_PIN 16
+
+// Used for sending and receiving Modbus commands
+HardwareSerial ModbusSerial(1);
+
+// Receive and transmit pins for the MAX485
+const int MODBUS_RX = 16;
+const int MODBUS_TX = 17;
+const int MODBUS_ENABLE = 18; // automatically set to high when writing, low otherwise to receive
+
+const int PUMP_ADDRESS = 0xEF; // Modbus address of pump controller
+const int MODBUS_TIMEOUT = 500; // timeout in ms for Modbus command responses
