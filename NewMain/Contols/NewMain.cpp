b0227586ed567f2c83849517585030a9e682a897
Gibby <gibsonmoseley@gmail.com>
2024-10-31 10:21:32 -0700

    Adding pin definitions

--- NewMain/Contols/NewMain.cpp
+++ NewMain/Contols/NewMain.cpp
@@ -1,5 +1,16 @@
-/****************************************************************************************************
- * NewMain.cpp
- * 
- * Function to keep bare neceties of the program running. For testing and to be eventually built out
+/*****************************************************************************************************
+* NewMain.cpp
+* 
+* Function to keep bare neceties of the program running. For testing and to be eventually built out
 /*****************************************************************************************************/
+
+
+#include <Arduino.h>
+#include <Wire.h>
+
+// Pin Definitions
+#define FLOW_SENSOR_1_PIN 12
+#define FLOW_SENSOR_2_PIN 13
+#define PUMP_PIN 14
+#define STEPPER_STEP_PIN 15
+#define STEPPER_DIR_PIN 16
