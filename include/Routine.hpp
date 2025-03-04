b2375814c357b51be14ea9575648eeb04a20a8bd
Gibby <gibsonmoseley@gmail.com>
2025-03-04 10:57:16 -0800

    Update Routine.hpp

--- include/Routine.hpp
+++ include/Routine.hpp
@@ -7,38 +7,38 @@ Gibson Moseley - Routine.hpp
 #include "BioreactorVaribiles.hpp"
 #include "FlowSensor.hpp"
 
-//using namespace std;
-//using namespace std::chrono;
+using namespace std;
+using namespace std::chrono;
 
-//int timeRunningTotal;
-//int timeRunningRoutine;
+int timeRunningTotal;
+int timeRunningRoutine;
 
 //alows seeting for a basic routne that repeats x times in hr conversions
-//void setRoutine(double timeRun, double timeBreak, double shearStress, int repetion) {
-    //int flowRate = flowRateBasedOnShearStressCalc(shearStress);
-    //auto timeRunHr =  std::chrono::duration<double, std::chrono::hours::period>(timeRun);
-    //auto timeBreakHr = std::chrono::duration<double, std::chrono::hours::period>(timeBreak);
+void setRoutine(double timeRun, double timeBreak, double shearStress, int repetion) {
+    int flowRate = flowRateBasedOnShearStressCalc(shearStress);
+    auto timeRunHr =  std::chrono::duration<double, std::chrono::hours::period>(timeRun);
+    auto timeBreakHr = std::chrono::duration<double, std::chrono::hours::period>(timeBreak);
 
-    // for(int i = 0; i < repetion; i++) {
-    //     checkPumpStatus();
-    //     if(pumpOn == 1) {
-    //         setPump(0);
-    //     }
+    for(int i = 0; i < repetion; i++) {
+        checkPumpStatus();
+        if(pumpOn == 1) {
+            setPump(0);
+        }
 
-    //     setPumpSpeed(flowRate, false);
-    //     setPump(1);
+        setPumpSpeed(flowRate, false);
+        setPump(1);
 
-    //     auto start = high_resolution_clock::now();
-    //     while (duration_cast<hours>(high_resolution_clock::now() - start) < timeRunHr) { //when time run is set turn pump off and keep off for time break amount
-    //         readFlowSensor(flowSensor);
-    //         start = high_resolution_clock::now();
-    //     }
+        auto start = high_resolution_clock::now();
+        while (duration_cast<hours>(high_resolution_clock::now() - start) < timeRunHr) { //when time run is set turn pump off and keep off for time break amount
+            readFlowSensor(flowSensor);
+            start = high_resolution_clock::now();
+        }
 
-    //     setPump(0);
-    //     start = high_resolution_clock::now();
-    //     while (duration_cast<hours>(high_resolution_clock::now() - start) < timeRunHr) { //when time run is set turn pump off and keep off for time break amount
-    //         readFlowSensor(flowSensor);
-    //         start = high_resolution_clock::now();
-    //     }
-    //}
-//}
+        setPump(0);
+        start = high_resolution_clock::now();
+        while (duration_cast<hours>(high_resolution_clock::now() - start) < timeRunHr) { //when time run is set turn pump off and keep off for time break amount
+            readFlowSensor(flowSensor);
+            start = high_resolution_clock::now();
+        }
+    }
+}
