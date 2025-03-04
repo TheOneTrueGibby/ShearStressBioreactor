bcd878b48a6b496b4c4fa82d41e8d3d245ffec69
Gibby <gibsonmoseley@gmail.com>
2025-03-04 11:12:51 -0800

    Update BioreactorVaribiles.hpp

--- include/BioreactorVaribiles.hpp
+++ include/BioreactorVaribiles.hpp
@@ -1,15 +1,19 @@
 /************************************************************************
 Gibson Moseley - BioreactorVaribiles.hpp
+
+This holds all equations and varbilies about bioreactor setup to claulate values correctly
 *************************************************************************/
 
 #ifndef BIOREACTORVARIBILES_HPP
 #define BIOREACTORVARIBILES_HPP
 
+//All bioreactor varibiles needed for calculation
 static constexpr double CHANNEL_HEIGHT = 0.0005; // m
 static constexpr double CHANNEL_WIDTH = 0.04; // m
 static constexpr double MU = 0.0006922; // Pa * s
 static constexpr double RHO = 993; // kg / m^3
 
+//Calculates flowrate
 double flowRateCalc(double sesnorValue) {
     double fl = (((CHANNEL_WIDTH * CHANNEL_HEIGHT * CHANNEL_HEIGHT * sesnorValue) / (6 * MU)) * 6e7)/32;
     return fl;
