66ba84bf35ab9d7ad4dc7a1fb4a2c82e6b858fa8
Gibby <gibsonmoseley@gmail.com>
2025-03-06 16:48:37 -0800

    Update Routine.hpp

--- include/Routine.hpp
+++ include/Routine.hpp
@@ -15,7 +15,8 @@ using namespace std::chrono;
 int timeRunningTotal;
 int timeRunningRoutine;
 
-//alows seeting for a basic routne that repeats x times in hr conversions
+//alows seeting for a basic routne that repeats x times in hr conversions, scaled in seconds and rounds.
+//Example 3.6 seconds will be 4 seconds
 void setRoutine(double timeRun, double timeBreak, double shearStress, int repetion) {
     int flowRate = flowRateBasedOnShearStressCalc(shearStress);
     auto timeRunHr =  std::chrono::duration<double, std::chrono::seconds::period>(timeRun * 3600);
@@ -32,7 +33,7 @@ void setRoutine(double timeRun, double timeBreak, double shearStress, int repeti
             setPumpSpeed(flowRate, 1);
         }
 
-        Serial.printf("Starting Routine Iternation: %d\n", i);
+        Serial.printf("Starting Routine Iternation: %d, ", i);
         setPump(1);
 
         auto start = high_resolution_clock::now();
@@ -42,7 +43,7 @@ void setRoutine(double timeRun, double timeBreak, double shearStress, int repeti
         }
         auto finalTime = duration_cast<seconds>(high_resolution_clock::now() - start);
         int time = finalTime.count();
-        Serial.printf("Time ran was %d seconds\n", time);
+        Serial.printf("Time ran was %d seconds, ", time);
 
         setPump(0);
         start = high_resolution_clock::now();
@@ -52,6 +53,7 @@ void setRoutine(double timeRun, double timeBreak, double shearStress, int repeti
         }
         finalTime = duration_cast<seconds>(high_resolution_clock::now() - start);
         time = finalTime.count();
-        Serial.printf("Time paused was %d seconds\n", time);
+        Serial.printf("Time paused was %d seconds.\n", time);
     }
+    Serial.print("Routine Done\n"); 
 }
