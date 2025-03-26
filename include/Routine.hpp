b35a28acc2bf272f475f73486476194ab9b66684
Gibson Moseley <gibsonmoseley@gmail.com>
2025-03-26 11:20:45 -0700

    Fixing Clock Concersion

--- include/Routine.hpp
+++ include/Routine.hpp
@@ -23,31 +23,19 @@ using namespace std::chrono;
 //this converts the amount of time running in seconds to a String with Hr:Min:Sec
 String convertTimeToString(int timeSeconds) {
     int timeLeft = timeSeconds;
-    int totalHr = 0;
-    int totalMin = 0;
-    int totalSec = 0;
-    Serial.print(timeLeft);
-    Serial.print("\n");
+
+    int totalHr = timeSeconds / 3600;
+    int totalMin = (timeSeconds % 3600) / 60;
+    int totalSec = timeSeconds % 60;
+
     String clockConversion = "";
-
-    if(timeLeft/3600 >= 1) {
-        totalHr = timeLeft/3600;
-        timeLeft -= timeLeft - (3600 * totalHr);
-    }
-
-    if(timeLeft/60 >= 1) {
-        totalMin = timeLeft/60;
-        timeLeft -= timeLeft - (60 * totalMin);
-    }
-
-    totalSec = timeLeft;
 
     clockConversion = "Routine Time: " + String(totalHr) + ":" + String(totalMin) + ":" + String(totalSec);
 
     String clockWebsite = "runningTime; " + clockConversion;
-    Serial.print(clockConversion);
     ws.textAll(clockWebsite);
-    Serial.print("\n");
+    //Serial.print(clockConversion);
+    //Serial.print("\n");
 
     return clockConversion;
     
