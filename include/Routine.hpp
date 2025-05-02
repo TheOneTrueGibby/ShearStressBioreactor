c4d317314d3406fc641077055c08c18206132106
Gibby <gibsonmoseley@gmail.com>
2025-05-02 15:01:08 -0700

    Update Routine Comments

--- include/Routine.hpp
+++ include/Routine.hpp
@@ -52,37 +52,37 @@ void setRoutine(String routineName, float timeRun, float timeBreak, float shearS
     //Update vars on website
    pushCurrentVariables();
 
-    //calculate flowrate needed to achive required shearStress
+    //Calculate flowrate needed to achive required shearStress
     float flowRate = flowRateBasedOnShearStressCalc(shearStress);
 
-    //scale the time to run and break from hr to seconds
+    //Scale the time to run and break from hr to seconds
     auto timeRunHr =  std::chrono::duration<float, std::chrono::seconds::period>(timeRun * 3600);
     auto timeBreakHr = std::chrono::duration<float, std::chrono::seconds::period>(timeBreak * 3600);
 
     //Print name of routinr that is being run
-    //Serial.printf("Running Routine: %s\n", routineName);
+    // Serial.printf("Running Routine: %s\n", routineName);
 
-    //clock to calculate total time of the routine run and when samples were read
+    //Clock to calculate total time of the routine run and when samples were read
     auto startTotal = high_resolution_clock::now();
 
-    //runn the routine as may times as speicifed
+    //Run the routine as may times as speicifed
     for(int i = 1; i < repetion + 1; i++) {
         checkPumpStatus(0);
 
         //make sure flow rate is set and pump is off, and start pump speed at 8
-        if (pumpOn == 1 & i == 0) {
+        if (pumpOn == 1 && i == 0) {
             setPump(0);
             setPumpSpeed(8, 1);
         }
-        else if (pumpOn == 0 & i == 0) {
+        else if (pumpOn == 0 && i == 0) {
             setPumpSpeed(8, 1);
         }
 
-        //print what repition we are on and start pump
-        //Serial.printf("Starting Routine Iteration: %d\n", i);
+        //Print what repition we are on and start pump
+        // Serial.printf("Starting Routine Iteration: %d\n", i);
         setPump(1);
 
-        //run the pump for specified amount of run time
+        //Run the pump for specified amount of run time
         auto start = high_resolution_clock::now();
         while (duration_cast<seconds>(high_resolution_clock::now() - start) < timeRunHr) {
             delay(1000); //Run all data only every second
@@ -96,10 +96,10 @@ void setRoutine(String routineName, float timeRun, float timeBreak, float shearS
             writeBioreactorInfo(routineName, time, flow, pump); //Write all data to microSD
         }
 
-        //for break turn pump off
+        //For break turn pump off
         setPump(0);
 
-        //dont run the pump for the speicifed amount of break time
+        //Do not run the pump for the speicifed amount of break time
         start = high_resolution_clock::now();
         auto lastPumpRun = start; // Track the last time the pump ran during the break
         while (duration_cast<seconds>(high_resolution_clock::now() - start) < timeBreakHr) {
@@ -112,13 +112,13 @@ void setRoutine(String routineName, float timeRun, float timeBreak, float shearS
 
             writeBioreactorInfo(routineName, time, flow, pump); //Write all data to microSD
 
-            // TESTING: Refresh the media in the chamber every few hours during the break period
-            // Check if it's time to run the pump at the lowest speed
+            //TESTING: Refresh the media in the chamber every few hours during the break period
+            //Check if it's time to run the pump at the lowest speed
             if (duration_cast<seconds>(high_resolution_clock::now() - lastPumpRun) >= seconds(4*3600)) { // Run every 4 hours
-                setPumpSpeed(8, true); // Set pump to the lowest speed (~8 ml/min)
-                setPump(1); // Turn on the pump
-                delay(120000); // Run the pump for 2 minutes (120,000 ms)
-                setPump(0); // Turn off the pump
+                setPumpSpeed(8, true); //Set pump to the lowest speed (~8 ml/min)
+                setPump(1); //Turn on the pump
+                delay(120000); //Run the pump for 2 minutes (120,000 ms)
+                setPump(0); //Turn off the pump
                 lastPumpRun = high_resolution_clock::now(); // Update the last pump run time
             }
         }
